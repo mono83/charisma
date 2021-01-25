1b5381b2abb4f526b26033400e5d4b32967d8f29
Gotterdemarung <gotterdemarung@gmail.com>
2021-01-25 20:47:48 +0200

    :sparkles: Protected utility methods

--- src/main/java/com/github/mono83/charisma/StdMutableValuesContainer.java
+++ src/main/java/com/github/mono83/charisma/StdMutableValuesContainer.java
@@ -1,6 +1,7 @@
 package com.github.mono83.charisma;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Standard implementation of value container.
@@ -28,5 +29,58 @@ public abstract class StdMutableValuesContainer<T extends Enum<T>> {
      */
     protected StdMutableValuesContainer(final Map<T, Long> source) {
         this.values = MutableValues.from(source);
+    }
+
+    /**
+     * Returns value, associated with key.
+     *
+     * @param key Key to match.
+     * @return Associated value.
+     */
+    protected Optional<Long> get(final T key) {
+        return values.get(key);
+    }
+
+    /**
+     * Returns value, associated with key.
+     *
+     * @param key Key to match.
+     * @return Associated value.
+     * @throws KeyMissingException If no key found.
+     */
+    protected long mustGet(final T key) throws KeyMissingException {
+        return values.mustGet(key);
+    }
+
+    /**
+     * Returns value associated with key or default value, if there is no value.
+     *
+     * @param key          Key to match.
+     * @param defaultValue Default value.
+     * @return Resulting value.
+     */
+    protected long get(final T key, final long defaultValue) {
+        return values.get(key, defaultValue);
+    }
+
+    /**
+     * Inserts or replaces value.
+     *
+     * @param key   Key to match.
+     * @param value Value to set.
+     * @return Previous value or null if none.
+     */
+    protected Long set(final T key, long value) {
+        return values.set(key, value);
+    }
+
+    /**
+     * Inserts or replaces multiple values atomically.
+     *
+     * @param pairs Key value pairs.
+     * @return Map of previous values for modified keys.
+     */
+    protected Map<T, Long> set(final Map<T, Long> pairs) {
+        return values.set(pairs);
     }
 }
--- src/main/java/com/github/mono83/charisma/StdValuesContainer.java
+++ src/main/java/com/github/mono83/charisma/StdValuesContainer.java
@@ -1,6 +1,7 @@
 package com.github.mono83.charisma;
 
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Standard implementation of value container.
@@ -26,5 +27,37 @@ public abstract class StdValuesContainer<T extends Enum<T>> {
      */
     protected StdValuesContainer(final Map<T, Long> source) {
         this.values = Values.from(source);
+    }
+
+    /**
+     * Returns value, associated with key.
+     *
+     * @param key Key to match.
+     * @return Associated value.
+     */
+    protected Optional<Long> get(final T key) {
+        return values.get(key);
+    }
+
+    /**
+     * Returns value, associated with key.
+     *
+     * @param key Key to match.
+     * @return Associated value.
+     * @throws KeyMissingException If no key found.
+     */
+    protected long mustGet(final T key) throws KeyMissingException {
+        return values.mustGet(key);
+    }
+
+    /**
+     * Returns value associated with key or default value, if there is no value.
+     *
+     * @param key          Key to match.
+     * @param defaultValue Default value.
+     * @return Resulting value.
+     */
+    protected long get(final T key, final long defaultValue) {
+        return values.get(key, defaultValue);
     }
 }
